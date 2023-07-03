ef773ae3bde5002822482fa7a5a108c913b7511b
marcinWojtczak <marcinwojtczak.pure@gmail.com>
2023-07-03 16:56:22 +0200

    m

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -54,7 +54,7 @@ const Form = () => {
       { !isSubmit ? (
       <form
         onSubmit={onSubmit}
-        action='http://formspree.io/marcinwojtczak.pure@gmail.com'
+        action='https://formspree.io/f/meqbjwyz'
         method="POST"
         className='py-10 flex flex-col items-center basis-1/2'
       >
