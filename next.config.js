982d1bedf4883236902544b7705f3558e4fbefd4
marcinWojtczak <marcinwojtczak.pure@gmail.com>
2023-07-03 14:23:01 +0200

    commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
-  output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
+  // output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
+  output: export,
 }
 
 module.exports = nextConfig
